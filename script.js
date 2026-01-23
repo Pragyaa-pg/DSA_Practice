b30d0a0d0291a1e7db34e91311d15b87c77570e3
Pragyaa-pg <aaryapg17@gmail.com>
2026-01-23 12:56:12 +0530

    add demo files on git(practice part)

---
+++ script.js
@@ -0,0 +1,15 @@
+let btn1 = document.querySelector("#btn1");
+
+btn1.onclick =() =>{
+    console.log("btn1 was clicked!");
+    let a = 25;
+    console.log(a);
+}
+
+let btn2 =document.querySelector("#btn2");
+
+btn2.ondblclick =() =>{
+    console.log("button was clicked twice");
+    let b=26;
+    console.log("b:"+b);
+}
