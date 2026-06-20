12456c1371a3ac92cfe15f2dcecbb10a86bead34
Pragyaa-pg <aaryapg17@gmail.com>
2026-06-20 20:06:22 +0530

    add question practice of string

---
+++ randomPracticeQuestions/stringPractice.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class stringPractice {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter string:");
+        String name = sc.nextLine();
+
+        
+        System.out.println(name.toLowerCase());
+
+    }
+    
+}
